fdb347f73232114625781cd496db3106cbd04869
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-08-19 16:10:26 +0100

    [Add] Adding and populating CardsGrid

---
+++ Assets/Scripts/CardsGrid.cs
@@ -0,0 +1,39 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using Vector2 = UnityEngine.Vector2;
+
+public class CardsGrid : MonoBehaviour
+{
+    [SerializeField] private Card cardPrefab;
+    [SerializeField] private GridLayoutGroup grid;
+    [SerializeField] private int rows = 2;
+    [SerializeField] private int columns = 2;
+
+    private void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        PopulateGrid();
+    }
+
+    private void PopulateGrid()
+    {
+        CalculateCellSize();
+        var cardsCount = rows * columns;
+        for (int i = 0; i < cardsCount; i++)
+        {
+            var newCard = Instantiate(cardPrefab,grid.transform);
+            
+        }
+    }
+
+    private void CalculateCellSize()
+    {
+        var gridRectTransform = grid.GetComponent<RectTransform>();
+        var gridWidth = gridRectTransform.rect.width-grid.padding.left-grid.padding.right-(columns-1)*grid.spacing.x;
+        Debug.Log("Width: "+ gridWidth);
+        var gridHeight = gridRectTransform.rect.height-grid.padding.top-grid.padding.bottom-(rows-1)*grid.spacing.y;;
+        Debug.Log("Height: "+ gridHeight);
+        grid.cellSize = new Vector2(gridWidth / columns, gridHeight / rows);
+    }
+}
