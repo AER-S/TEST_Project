c960048d22a1a40cee280f530997a970af163abc
Alaeddine_Rekik <alaa.eddine.rekik@gmail.com>
2024-08-20 04:58:20 +0100

    [Add] Adding Panels Manager to the scene

---
+++ Assets/Scripts/PanelsManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Common;
+using UnityEngine;
+
+public class PanelsManager : Singleton<PanelsManager>
+{
+    [SerializeField] private RectTransform mainMenuPanel;
+    [SerializeField] private RectTransform gamePanel;
+    [SerializeField] private RectTransform gameOverPanel;
+
+    private void HideAll()
+    {
+        gamePanel.gameObject.SetActive(false);
+        gameOverPanel.gameObject.SetActive(false);
+        mainMenuPanel.gameObject.SetActive(false);
+    }
+
+    public void ShowMainMenuPanel()
+    {
+        HideAll();
+        mainMenuPanel.gameObject.SetActive(true);
+    }
+    
+    public void ShowGamePanel()
+    {
+        HideAll();
+        gamePanel.gameObject.SetActive(true);
+    }
+
+    public void ShowGameOverPanel()
+    {
+        HideAll();
+        gameOverPanel.gameObject.SetActive(true);
+    }
+}
